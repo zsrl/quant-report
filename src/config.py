2dc60fe2ff4d82a399a0c9d8e9473cab98a880c4
pluto <mayuanchi1029@gmail.com>
2023-07-09 15:12:02 +0800

    fix: 增加指数详情

--- src/config.py
+++ src/config.py
@@ -1,18 +1,70 @@
 config = {
     'T+0':{
-        '513050.SH': '中概互联',
-        '513330.SH': '恒生互联',
-        '513060.SH': '恒生医疗',
-        '513180.SH': '恒生科技（华夏）',
-        '513130.SH': '恒生科技（广发）',
-        '159920.SZ': '恒生',
-        '510900.SH': 'H股ETF',
-        '513100.SH': '纳指ETF',
-        '513500.SH': '标普500',
-        '513660.SH': '恒生通',
-        '513550.SH': '港股通50',
-        '159605.SZ': '中概互联30',
-        '518880.SH': '黄金ETF'
+        '513050.SH': {
+            'name': '中概互联',
+            'fund_company': '易方达',
+            'index_url': 'https://www.csindex.com.cn/#/indices/family/detail?indexCode=H30533',
+        },
+        '159605.SZ': {
+            'name': '中概互联30',
+            'fund_company': '广发',
+            'index_url': 'https://www.csindex.com.cn/#/indices/family/detail?indexCode=930604',
+        },
+        '513330.SH': {
+            'name': '恒生互联',
+            'fund_company': '华夏',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hsiii',
+        },
+        '513060.SH': {
+            'name': '恒生医疗',
+            'fund_company': '博时',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hshci',
+        },
+        '513180.SH': {
+            'name': '恒生科技',
+            'fund_company': '华夏',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hstech',
+        },
+        '513130.SH': {
+            'name': '恒生科技',
+            'fund_company': '广发',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hstech',
+        },
+        '159920.SZ': {
+            'name': '恒生',
+            'fund_company': '华夏',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hsi',
+        },
+        '513660.SH': {
+            'name': '恒生通',
+            'fund_company': '华夏',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hsi',
+        },
+        '510900.SH': {
+            'name': 'H股ETF',
+            'fund_company': '易方达',
+            'index_url': 'https://www.hsi.com.hk/schi/indexes/all-indexes/hscei',
+        },
+        '513100.SH': {
+            'name': '纳指ETF',
+            'fund_company': '国泰',
+            'index_url': 'https://www.nasdaq.com/solutions/nasdaq-100',
+        },
+        '513500.SH': {
+            'name': '标普500',
+            'fund_company': '博时',
+            'index_url': 'https://www.spglobal.com/spdji/zh/indices/equity/sp-500/',
+        },
+        '513550.SH': {
+            'name': '港股通50',
+            'fund_company': '华泰柏瑞',
+            'index_url': 'https://www.csindex.com.cn/#/indices/family/detail?indexCode=930931',
+        },
+        '518880.SH': {
+            'name': '黄金ETF',
+            'fund_company': '华安',
+            'index_url': 'https://www.sge.com.cn/sjzx/mrhq',
+        } 
     },
     # 'T+1': {
     #     '000300.SH': '沪深300ETF'
